c968a89db884f694b2b31166b9e3866710f4f28f
HasegawaAce <corentin.bruant.49@gmail.com>
2017-04-27 22:18:02 +0200

    corection bug

--- ui/src/main/java/fr/corentin/ui/game/Plateau.java
+++ ui/src/main/java/fr/corentin/ui/game/Plateau.java
@@ -148,12 +148,12 @@ public class Plateau extends JPanel implements ActionListener {
 			Rectangle monsterHitBox = monster.getBounds();
 			Rectangle monsterObservationBox = monster.getObservationBounds();
 			
-			if (playerHitBox.intersects(monsterHitBox) || monsterHitBox.intersects(playerHitBox)) {
+			if (playerHitBox.intersects(monsterHitBox)) {
 				player.setX(player.getX() + player.getDx() * -50);
 				player.lostLife(1);
 			}
 			
-			if (playerHitBox.intersects(monsterHitBox) || monsterHitBox.intersects(playerHitBox)) {
+			if (playerHitBox.intersects(monsterHitBox)) {
 				player.setY(player.getY() + player.getDy() * -50);
 			}
 			
